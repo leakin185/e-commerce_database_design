20f5819dd74c03113b2a2a2fa5d2b2e45bb4b05a
leakin185 <65524912+leakin185@users.noreply.github.com>
2022-03-27 14:58:04 +0800

    Create Additional Query Solutions.sql

---
+++ Additional Query Solutions.sql
@@ -0,0 +1,8 @@
+-- 1. For each shop, come up with its product development strategies
+-- find the product per category which produces the lowest revenue 
+-- Find the product per category which produces the highest revenue 
+
+-- 2. For each shop, find the most loyal customer 
+-- The definition of the most loyal customer is one who spends the most amount of money in the shop AND ordered the highest amount in the shop AND makes the highest ratings in the shop
+
+-- 3. Find the shops whose customer service quality (rating) worsened the most over 3 months
